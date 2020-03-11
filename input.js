78d4a6c7c445c2bbfb7c8908f3f7c2ae7ac7953f
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-11 18:51:26 +0000

    add movement from w, a, s, d input

--- input.js
+++ input.js
@@ -1,3 +1,6 @@
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
@@ -5,16 +8,26 @@
 //function to handle input (establish UI) 
   //allow CTRL+C to exit game
 
-  const handleUserInput = (input) => {
-    if (input === '\u0003') process.exit();
-  }
-  const setupInput = function() {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-    stdin.on('data', handleUserInput);
-    return stdin;
-  }
+const handleUserInput = (input) => {
+  if (input === '\u0003') { 
+    process.exit();
+  } else if (input === 'w') {
+    connection.write("Move: up"); 
+  } else if (input === 'a') {
+    connection.write("Move: left"); 
+  } else if (input === 's') {
+    connection.write("Move: down");
+  } else if (input === 'd') connection.write("Move: right");
+};
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
   
 module.exports = setupInput;
