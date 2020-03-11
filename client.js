fbb20d9eac8dfcfba57dfc7f2e38a84a0389ee70
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-11 18:03:27 +0000

    commented code and added function to handle user input, specifically ^C to exit game

--- client.js
+++ client.js
@@ -5,18 +5,21 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
+    host: '172.46.0.203',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  //get statement from server and print
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+  //when connection established print statement
   conn.on('connect', () => {
     console.log("Successfully connected to game server")
   });
+  //send server name for user's snake
   conn.on('connect', () => {
     conn.write("Name: KJG")
   });
